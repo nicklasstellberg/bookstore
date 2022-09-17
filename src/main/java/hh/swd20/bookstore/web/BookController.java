7427d848f83de91a6dbc9514943453bd06b990d2
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-09-17 12:21:00 +0300

    Listpage

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,24 +1,19 @@
 package hh.swd20.bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-		@RequestMapping(value = "/index", method = RequestMethod.GET)
-		public String getAutot(Model model) {
-			List<Book> books = new ArrayList<Book>();
-			books.add(new Book("Villahullu", "Heli Nikula", 2022, "9789523734319", 22.90));
-			books.add(new Book("Sitan kalenteri 2022-2023", "Sita Salminen", 2022, "6430060036024", 21.90));
-			model.addAttribute("books", books);
-			
-			return "booklist";
-		}
+	@Autowired
+	private BookRepository repository; 
+	@RequestMapping(value="/booklist")
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 }
