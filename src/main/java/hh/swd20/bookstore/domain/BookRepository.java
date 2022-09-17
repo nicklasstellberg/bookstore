1616993a391b10335f59d68abb12ab821daf00a4
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-09-17 18:09:19 +0300

    Added add function

--- src/main/java/hh/swd20/bookstore/domain/BookRepository.java
+++ src/main/java/hh/swd20/bookstore/domain/BookRepository.java
@@ -1,10 +1,9 @@
 package hh.swd20.bookstore.domain;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 	
-	List<Book> findByTitle(String title);
+	
 }
