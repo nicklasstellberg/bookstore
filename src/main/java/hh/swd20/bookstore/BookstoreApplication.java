3145a1371e016963be34b8137a245d70dae48f7f
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-09-25 13:27:21 +0300

    Added relation with book and category

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -28,8 +28,8 @@ public class BookstoreApplication {
 			crepository.save(category2);
 			Category category3 = new Category("Law");
 			crepository.save(category3);
-			repository.save(new Book("Villahullu", "Heli Nikula", 2022, "9789523734319", 22.90));
-			repository.save(new Book("Sitan kalenteri 2022-2023", "Sita Salminen", 2022, "6430060036024", 21.90));
+			repository.save(new Book("Villahullu", "Heli Nikula", 2022, "9789523734319", 22.90, category1));
+			repository.save(new Book("Sitan kalenteri 2022-2023", "Sita Salminen", 2022, "6430060036024", 21.90, category2));
 			log.info("Saved some sample categories");
 			log.info("Saved some sample books");
 			log.info("Fetch all categories");
--- src/main/java/hh/swd20/bookstore/domain/Category.java
+++ src/main/java/hh/swd20/bookstore/domain/Category.java
@@ -1,9 +1,13 @@
 package hh.swd20.bookstore.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Category {
@@ -11,6 +15,9 @@ public class Category {
     @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long categoryid;
 	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+	private List<Book> books;
 	
 	public Category() {}
 	
@@ -33,6 +40,14 @@ public class Category {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
 	}
 
 	@Override
