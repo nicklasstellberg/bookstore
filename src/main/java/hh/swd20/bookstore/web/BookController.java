9a9404bcf02f0dd3fa58247dec49e4fa0e5c2b23
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-10-03 21:46:08 +0300

    Added Restful service for saving book

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,9 +39,13 @@ public class BookController {
 	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
     	return repository.findById(bookId);
-    }   
-
+    } 
 	
+	@RequestMapping(value="/books", method = RequestMethod.POST)
+    public @ResponseBody Book saveBookRest(@RequestBody Book book) {	
+    	return repository.save(book);
+    }
+
 	@RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
